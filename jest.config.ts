379b15af2fc0894cdf671f5b798f61649d2ca01e
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2020-10-27 13:03:56 +0100

    Set up testing

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
+  /* It is the default option, but we have to declare it because we use "babel/next"
+   pre-processor */
+  transform: { "\\.[jt]sx?$": "babel-jest" },
+  // We use identity-obj-proxy to mock css modules
+  moduleNameMapper: { "\\.(css|less)$": "identity-obj-proxy"},
+  clearMocks: true,
+  coverageDirectory: '/__coverage__'
+}
+
+export default config;
