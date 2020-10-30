86b51f7f983d49c81d2436668eac6f7a5ca822dd
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2020-10-30 17:22:48 +0100

    Header appName is now a link when not in "/" path

--- client/clientShared/Header.test.tsx
+++ client/clientShared/Header.test.tsx
@@ -1,13 +1,52 @@
 import { render } from "@testing-library/react"
 import appName from "../../appShared/appName"
 import Header, { appDescription } from "./Header"
+import userEvent from "@testing-library/user-event"
 
-it("should show the app name and description", () => {
-  const { getByText } = render(<Header />)
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      pathname: "/",
+      push: mockPush,
+    }
+  },
+}))
 
-  const appNameElement = getByText(appName)
-  const appDescriptionElement = getByText(appDescription)
+const mockPush = jest.fn()
 
-  expect(appNameElement).toBeInTheDocument()
-  expect(appDescriptionElement).toBeInTheDocument()
+describe("Header", () => {
+  it("should show the app name and description", () => {
+    const { getByText } = render(<Header />)
+
+    const appNameElement = getByText(appName)
+    const appDescriptionElement = getByText(appDescription)
+
+    expect(appNameElement).toBeInTheDocument()
+    expect(appDescriptionElement).toBeInTheDocument()
+  })
+
+  test("the page title should not be a link if we are in the home page", () => {
+    const { getByText } = render(<Header />)
+    const appNameElement = getByText(appName)
+
+    userEvent.click(appNameElement)
+
+    expect(mockPush).not.toHaveBeenCalled()
+    expect(appNameElement).not.toHaveStyle("cursor: pointer")
+  })
+
+  test("the page title should be a link to go back to the home page if we are not in there", () => {
+    jest.spyOn(require("next/router"), "useRouter").mockImplementation(() => ({
+      pathname: "/login",
+      push: mockPush,
+    }))
+
+    const { getByText } = render(<Header />)
+    const appNameElement = getByText(appName)
+
+    userEvent.click(appNameElement)
+
+    expect(mockPush).toHaveBeenCalledWith("/")
+    expect(appNameElement).toHaveStyle("cursor: pointer")
+  })
 })
--- client/clientShared/Header.tsx
+++ client/clientShared/Header.tsx
@@ -1,4 +1,5 @@
 import { makeStyles, Typography } from "@material-ui/core"
+import { useRouter } from "next/router"
 import appName from "../../appShared/appName"
 
 export const appDescription = "Gestiona tu trabajo sin dolores de cabeza"
@@ -7,6 +8,7 @@ const useStyles = makeStyles((theme) => ({
   header: {
     padding: "2vmax",
     background: theme.palette.primary.main,
+    borderBottom: `1px solid ${theme.palette.primary.dark}`,
   },
   headerText: {
     color: theme.palette.getContrastText(theme.palette.primary.main),
@@ -17,17 +19,30 @@ const useStyles = makeStyles((theme) => ({
       fontSize: "6rem",
     },
   },
+  cursor: {
+    cursor: "pointer",
+  },
 }))
 
 const Header = () => {
   const classes = useStyles()
+  const router = useRouter()
+
+  const pathIsNotHome = router.pathname !== "/"
+
+  const handleAppNameClick = () => {
+    if (pathIsNotHome) router.push("/")
+  }
 
   return (
     <header className={classes.header}>
       <Typography
         align="center"
         variant="h1"
-        className={`${classes.appName} ${classes.headerText}`}
+        className={`${classes.appName} ${classes.headerText} ${
+          pathIsNotHome ? classes.cursor : ""
+        }`}
+        onClick={handleAppNameClick}
       >
         {appName}
       </Typography>
