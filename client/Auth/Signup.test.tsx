25030a2bdddcf912b7739da99c3225dc6c62bb59
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2020-11-04 17:30:53 +0100

    Wrote tests for the SIgnup component

--- client/Auth/Signup.test.tsx
+++ client/Auth/Signup.test.tsx
@@ -9,4 +9,22 @@ describe("Signup", () => {
 
     expect(titleElement).toBeInTheDocument()
   })
+
+  describe("Form", () => {
+    it.todo("should have an email input")
+    test.todo("the email validation should fail with a random word")
+    test.todo("the email validation should pass with a valid email")
+    it.todo("should have a password input")
+    test.todo("the password validation should fail with less than 5 letters")
+    test.todo("the password validation should pass with 5 letters or more")
+    it.todo("should have a repeat password input")
+    test.todo("the repeatPassword field value should match the password value")
+    it.todo("should have a submit button")
+    it.todo("should have a link to go to the login page")
+    it.todo(
+      "should persist the form data in the local storage until the signup is completed"
+    )
+  })
+
+  //TODO: store
 })
