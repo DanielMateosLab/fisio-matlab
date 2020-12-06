0688fa67fc81c94dc4cae70c264890b3cf03128d
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2020-12-06 17:13:39 +0100

    Add password and repeatPassword validation unit tests

--- client/clientShared/Validation.test.tsx
+++ client/clientShared/Validation.test.tsx
@@ -1,9 +1,17 @@
 import * as yup from "yup"
-import { emailErrorText, emailValidation } from "./Validation"
+import {
+  emailErrorText,
+  emailValidation,
+  passwordValidation,
+  repeatPasswordErrorText,
+  repeatPasswordValidator,
+} from "./Validation"
+
+type TestString = string | undefined
 
 function expectValidationError(
   validator: yup.StringSchema,
-  testString: string,
+  testString: TestString,
   expectedError: string
 ) {
   expect(() => {
@@ -13,7 +21,7 @@ function expectValidationError(
 
 describe("email validation", () => {
   function expectEmailValidationError(
-    testString: string,
+    testString: TestString,
     expectedError: string
   ) {
     expectValidationError(emailValidation.validator, testString, expectedError)
@@ -25,7 +33,7 @@ describe("email validation", () => {
     expectEmailValidationError(testString, emailErrorText)
   })
   test("the email validation should fail with an empty email", () => {
-    const testString = ""
+    const testString = undefined
 
     expectEmailValidationError(testString, emailValidation.requiredErrorText)
   })
@@ -44,12 +52,54 @@ describe("email validation", () => {
 })
 
 describe("password validation", () => {
-  test.todo("the password validation should fail with no input")
-  test.todo(
-    "the password validation should fail with less characters than the minium length allowed"
-  )
-  test.todo(
-    "the password validation should fail with more characters than the maxium length allowed"
-  )
-  test.todo("the password validation should pass with a 20 letters password")
+  function expectPasswordValidationError(
+    testString: TestString,
+    expectedError: string
+  ) {
+    expectValidationError(
+      passwordValidation.validator,
+      testString,
+      expectedError
+    )
+  }
+  test("the password validation should fail with no input", () => {
+    const testString = undefined
+
+    expectPasswordValidationError(
+      testString,
+      passwordValidation.requiredErrorText
+    )
+  })
+  test("the password validation should fail with less characters than the minium length allowed", () => {
+    const testString = "a".repeat(passwordValidation.minCharacters - 1)
+
+    expectPasswordValidationError(testString, passwordValidation.minErrorText)
+  })
+  test("the password validation should fail with more characters than the maxium length allowed", () => {
+    const testString = "a".repeat(passwordValidation.maxCharacters + 1)
+
+    expectPasswordValidationError(testString, passwordValidation.maxErrorText)
+  })
+  test("the password validation should pass with a 20 letters password", () => {
+    const testString = "a".repeat(20)
+
+    expect(() =>
+      passwordValidation.validator.validateSync(testString)
+    ).not.toThrow()
+  })
+})
+
+describe("repeatPassword validation", () => {
+  it("should fail if the password is different", () => {
+    const password = "aaaa"
+    const repeatPassword = "bbbb"
+
+    const passwordsSchema = yup.object().shape({
+      password: yup.string(),
+      repeatPassword: repeatPasswordValidator,
+    })
+    expect(() => {
+      passwordsSchema.validateSync({ password, repeatPassword })
+    }).toThrowError(repeatPasswordErrorText)
+  })
 })
