38a76490020695e281fd3b7136fe36bc8fa46224
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2020-12-11 20:19:01 +0100

    Prepare Login tests

--- client/session/Login.test.tsx
+++ client/session/Login.test.tsx
@@ -1,9 +1,21 @@
-import { render } from "@testing-library/react"
+import { render, RenderResult } from "@testing-library/react"
 import Login, { loginTitle } from "./Login"
 
-it("should render the login page title", () => {
-  const { getByText } = render(<Login />)
-  const titleElement = getByText(loginTitle)
+describe("Login", () => {
+  let queries: RenderResult
 
-  expect(titleElement).toBeInTheDocument()
+  beforeEach(() => {
+    queries = render(<Login />)
+  })
+
+  it("should render the login page title", () => {
+    const { getByText } = queries
+    const titleElement = getByText(loginTitle)
+
+    expect(titleElement).toBeInTheDocument()
+  })
+
+  it.todo("should have an email input")
+  it.todo("should have a password input")
+  it.todo("should have a submit button")
 })
