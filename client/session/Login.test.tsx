3083b78e2e0ddebb51bdd4ff3656bedaf2af2b8f
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2020-12-12 11:41:31 +0100

    Create login page. Closes #5

--- client/session/Login.test.tsx
+++ client/session/Login.test.tsx
@@ -1,21 +1,98 @@
-import { render, RenderResult } from "@testing-library/react"
-import Login, { loginTitle } from "./Login"
+import { render, RenderResult, waitFor } from "@testing-library/react"
+import Login, {
+  loginTitle,
+  emailInputText,
+  passwordInputText,
+  submitButtonText,
+} from "./Login"
+import userEvent from "@testing-library/user-event"
+import { authSuccess } from "./sessionSlice"
+
+const mockDispatch = jest.fn()
+jest.mock("react-redux", () => ({
+  ...(jest.requireActual("react-redux") as {}),
+  useDispatch: () => mockDispatch,
+}))
+
+const mockPush = jest.fn()
+jest.mock("next/router", () => ({
+  useRouter: () => ({ push: mockPush }),
+}))
 
 describe("Login", () => {
   let queries: RenderResult
+  let getByLabelText: typeof queries.getByLabelText
 
   beforeEach(() => {
     queries = render(<Login />)
+    getByLabelText = queries.getByLabelText
   })
 
   it("should render the login page title", () => {
-    const { getByText } = queries
-    const titleElement = getByText(loginTitle)
+    const titleElement = queries.getByText(loginTitle)
 
     expect(titleElement).toBeInTheDocument()
   })
 
-  it.todo("should have an email input")
-  it.todo("should have a password input")
-  it.todo("should have a submit button")
+  it("should have an email input", () => {
+    const emailInputElement = getByLabelText(emailInputText)
+
+    expect(emailInputElement).toBeInTheDocument()
+  })
+  it("should have a password input", () => {
+    const passwordInputElement = getByLabelText(passwordInputText)
+
+    expect(passwordInputElement).toBeInTheDocument()
+  })
+  it("should have a submit button", () => {
+    const submitButtonElement = queries.getByRole("button", {
+      name: submitButtonText,
+    })
+
+    expect(submitButtonElement).toBeInTheDocument()
+  })
+  test("writing in the form should trigger validation", async () => {
+    const emailInputElement = getByLabelText(emailInputText)
+
+    userEvent.type(emailInputElement, "aaaa")
+    userEvent.tab()
+
+    await waitFor(() => {
+      expect(emailInputElement).toBeInvalid()
+    })
+  })
+  it("submitting the form should dispatch an action to save in the store the form values", async () => {
+    const emailElement = queries.getByLabelText(emailInputText)
+    const passwordElement = queries.getByLabelText(passwordInputText)
+    const submitButtonElement = queries.getByRole("button", {
+      name: submitButtonText,
+    })
+
+    const email = "aaaa@aaa.aa"
+    const pwd = "aaaaa"
+
+    userEvent.type(emailElement, email)
+    userEvent.type(passwordElement, pwd)
+    userEvent.click(submitButtonElement)
+
+    await waitFor(() => {
+      expect(mockDispatch).toHaveBeenCalledWith(authSuccess({ email }))
+    })
+  })
+  describe("signup page link", () => {
+    let signupPageLinkElement: HTMLElement
+
+    beforeEach(() => {
+      signupPageLinkElement = queries.getByText("Regístrate")
+    })
+
+    it("should have a link to go to the signup page", () => {
+      expect(signupPageLinkElement).toBeInTheDocument()
+    })
+    test("the link should redirect to the signup page", () => {
+      userEvent.click(signupPageLinkElement)
+
+      expect(mockPush).toHaveBeenCalledWith("/signup")
+    })
+  })
 })
--- client/session/Login.tsx
+++ client/session/Login.tsx
@@ -1,20 +1,92 @@
-import { makeStyles, Typography } from "@material-ui/core"
+import { Button, Link, makeStyles, Typography } from "@material-ui/core"
+import { Formik } from "formik"
+import { useRouter } from "next/router"
+import { useDispatch } from "react-redux"
+import FormikTextInput from "../clientShared/FormikTextInput"
 import PageTitle from "../clientShared/pageTitle"
+import { loginValidationSchema } from "../clientShared/Validation"
+import { authSuccess } from "./sessionSlice"
 
 export const loginTitle = "¿Quién eres?"
+export const emailInputText = "Tu email"
+export const passwordInputText = "Tu contraseña"
+export const submitButtonText = "Iniciar sesión"
 
 const useStyles = makeStyles((theme) => ({
   container: {
     padding: theme.spacing(1),
   },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  formElement: {
+    marginTop: theme.spacing(2),
+  },
 }))
 
 const Login = () => {
   const classes = useStyles()
+  const dispatch = useDispatch()
+  const router = useRouter()
+
+  function handleSignupPageLink(e: React.MouseEvent<HTMLSpanElement>) {
+    e.preventDefault()
+    router.push("/signup")
+  }
 
   return (
     <div className={classes.container}>
       <PageTitle> {loginTitle} </PageTitle>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={loginValidationSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          dispatch(authSuccess({ email: values.email }))
+          setSubmitting(false)
+        }}
+      >
+        {(formik) => (
+          <form onSubmit={formik.handleSubmit} className={classes.form}>
+            <div className={classes.formElement}>
+              <FormikTextInput
+                name="email"
+                label={emailInputText}
+                type="email"
+              />
+            </div>
+            <div className={classes.formElement}>
+              <FormikTextInput
+                name="password"
+                label={passwordInputText}
+                type="password"
+              />
+            </div>
+            <div className={classes.formElement}>
+              <Button
+                variant="contained"
+                color="secondary"
+                type="submit"
+                disabled={formik.isSubmitting}
+              >
+                {submitButtonText}
+              </Button>
+            </div>
+            <div className={classes.formElement}>
+              <Typography variant="body2">
+                ¿No tienes cuenta?{" "}
+                <Link href="#" onClick={handleSignupPageLink}>
+                  Regístrate
+                </Link>
+              </Typography>
+            </div>
+          </form>
+        )}
+      </Formik>
     </div>
   )
 }
