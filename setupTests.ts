5b705ee18a8d22c6c8ec3da087caf1d64d946364
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2020-12-13 15:12:03 +0100

    use css-mediaquery polyfill for jsdom

--- setupTests.ts
+++ setupTests.ts
@@ -3,3 +3,19 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect"
+
+import mediaQuery from "css-mediaquery"
+
+function createMatchMedia(width: number) {
+  return (query: any) => ({
+    matches: mediaQuery.match(query, { width }),
+    addListener: () => {},
+    removeListener: () => {},
+  })
+}
+
+describe("MyTests", () => {
+  beforeAll(() => {
+    window.matchMedia = createMatchMedia(window.innerWidth) as any
+  })
+})
