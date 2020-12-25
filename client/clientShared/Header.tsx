86d9d3285fd59c37a00b06f865668de8b1b06044
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2020-12-25 11:00:40 +0100

    Remove header margin

--- client/clientShared/Header.tsx
+++ client/clientShared/Header.tsx
@@ -14,7 +14,6 @@ const useStyles = makeStyles<Theme, { email: string; pathIsNotHome: boolean }>(
     header: {
       padding: ({ email }) => (email ? "1vmax" : "2vmax"),
       background: theme.palette.primary.light,
-      borderBottom: `1px solid ${theme.palette.primary.dark}`,
       display: ({ email }) => (email ? "flex" : "block"),
       alignItems: "center",
     },
