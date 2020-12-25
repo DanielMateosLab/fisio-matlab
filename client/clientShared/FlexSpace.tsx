67e78ded0bfa6e87b0107ab9f5ea1d00f36e8edc
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2020-12-25 11:35:23 +0100

    Add documentation to FlexSpace

--- client/clientShared/FlexSpace.tsx
+++ client/clientShared/FlexSpace.tsx
@@ -1,3 +1,6 @@
+/**
+ * A div with the flexGrow property set to 1. Useful to separe blocks in a flex line.
+ */
 const FlexSpace: React.FC = () => <div style={{ flexGrow: 1 }} />
 
 export default FlexSpace
