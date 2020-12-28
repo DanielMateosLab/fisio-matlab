0d6737fc1edecef8cafcc940ac6e5aa02e42c7cd
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2020-12-28 23:39:30 +0100

    Create change password form

--- client/session/Profile.test.tsx
+++ client/session/Profile.test.tsx
@@ -1,9 +1,17 @@
+import userEvent from "@testing-library/user-event"
 import {
   mockPush,
   render,
   renderAuthenticated,
+  RenderResult,
+  waitFor,
 } from "../clientShared/testUtils"
-import Profile from "./Profile"
+import { changePasswordValidationSchema } from "../clientShared/Validation"
+import Profile, {
+  currentPasswordInputText,
+  newPasswordInputText,
+  repeatNewPasswordInputText,
+} from "./Profile"
 
 describe("Me", () => {
   // Without session
@@ -14,9 +22,9 @@ describe("Me", () => {
   })
 
   // With session
-  it("should show the user email", () => {
-    const email = "aaaa@aaa.aa"
+  const email = "aaaa@aaa.aa"
 
+  it("should show the user email", () => {
     const { getByText } = renderAuthenticated(<Profile />, email)
     const emailElement = getByText(email)
 
@@ -24,9 +32,41 @@ describe("Me", () => {
   })
 
   describe("it should have a change-password form", () => {
-    it.todo('should have a "current password" field')
-    it.todo('should have a "new password" field')
-    it.todo('should have a "repeat new password" field')
+    let queries: RenderResult
+    beforeEach(() => {
+      queries = renderAuthenticated(<Profile />, email)
+    })
+    it('should have a "current password" field', () => {
+      const currentPwdElement = queries.getByLabelText(currentPasswordInputText)
+
+      expect(currentPwdElement).toBeInTheDocument()
+    })
+    it('should have a "new password" field', () => {
+      const newPwdElement = queries.getByLabelText(newPasswordInputText)
+
+      expect(newPwdElement).toBeInTheDocument()
+    })
+    it('should have a "repeat new password" field', () => {
+      const repeatPwdElement = queries.getByLabelText(
+        repeatNewPasswordInputText
+      )
+
+      expect(repeatPwdElement).toBeInTheDocument()
+    })
+    test("writing in the form should trigger validation", async () => {
+      expect.hasAssertions()
+      const validationSpy = jest.spyOn(
+        changePasswordValidationSchema,
+        "validate"
+      )
+
+      const currentPwdElement = queries.getByLabelText(currentPasswordInputText)
+      userEvent.type(currentPwdElement, "aaaa")
+
+      await waitFor(() => {
+        expect(validationSpy).toHaveBeenCalled()
+      })
+    })
     it.todo("should have a submit button")
   })
 
--- client/session/Profile.tsx
+++ client/session/Profile.tsx
@@ -1,12 +1,27 @@
-import { Container, makeStyles, Typography } from "@material-ui/core"
+import { Button, Container, makeStyles, Typography } from "@material-ui/core"
+import { Formik } from "formik"
+import FormikTextInput from "../clientShared/FormikTextInput"
 import useRedirectUnauth from "../clientShared/useRedirectUnauth"
+import { changePasswordValidationSchema } from "../clientShared/Validation"
 import { useTypedSelector } from "../redux/rootReducer"
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   container: {
     paddingTop: "16px",
   },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  formElement: {
+    marginTop: theme.spacing(2),
+  },
 }))
+
+export const currentPasswordInputText = "Contraseña actual"
+export const newPasswordInputText = "Nueva contraseña"
+export const repeatNewPasswordInputText = "Repite la nueva contraseña"
+export const submitButtonText = "Cambiar contraseña"
 
 const Profile: React.FC = () => {
   useRedirectUnauth()
@@ -19,6 +34,8 @@ const Profile: React.FC = () => {
     </Typography>
   )
 
+  // TODO: maybe make a grid layout to have two columns (it could just be flexbox)
+  // TODO: logout after successful password change. Kill all user sessions when changing pwd.
   return (
     <Container className={classes.container} maxWidth="sm" component="main">
       <Typography variant="h3" component="h2" gutterBottom>
@@ -35,6 +52,53 @@ const Profile: React.FC = () => {
         <Typography variant="body1">
           Aquí puedes cambiar la contraseña que utilizas para iniciar sesión.
         </Typography>
+        <Formik
+          initialValues={{
+            currentPassword: "",
+            password: "",
+            repeatPassword: "",
+          }}
+          validationSchema={changePasswordValidationSchema}
+          onSubmit={(values, { setSubmitting }) => {
+            setSubmitting(false)
+          }}
+        >
+          {(formik) => (
+            <form onSubmit={formik.handleSubmit} className={classes.form}>
+              <div className={classes.formElement}>
+                <FormikTextInput
+                  name="currentPassword"
+                  label={currentPasswordInputText}
+                  type="currentPassword"
+                />
+              </div>
+              <div className={classes.formElement}>
+                <FormikTextInput
+                  name="password"
+                  label={newPasswordInputText}
+                  type="password"
+                />
+              </div>
+              <div className={classes.formElement}>
+                <FormikTextInput
+                  name="repeatPassword"
+                  label={repeatNewPasswordInputText}
+                  type="password"
+                />
+              </div>
+              <div className={classes.formElement}>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  type="submit"
+                  disabled={formik.isSubmitting}
+                >
+                  {submitButtonText}
+                </Button>
+              </div>
+            </form>
+          )}
+        </Formik>
       </section>
     </Container>
   )
