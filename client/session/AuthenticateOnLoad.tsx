03adda4384260571d90354b6d03d73bb2c5d8053
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-01-01 19:13:00 +0100

    Redirect to profile after getting email from cookie

--- client/session/AuthenticateOnLoad.tsx
+++ client/session/AuthenticateOnLoad.tsx
@@ -1,18 +1,22 @@
-import { useDispatch } from "react-redux"
 import Cookies from "js-cookie"
 import { login, sessionCookieName } from "./sessionSlice"
 import { useEffect } from "react"
+import { useRouter } from "next/router"
+import { useThunkDispatch } from "../redux/store"
 
 // This component will in the future dispatch a getUser action, and will return a
 // "loading" component while session.getUserPending is true
 
 const AuthenticateOnLoad: React.FC = ({ children }) => {
-  const dispatch = useDispatch()
+  const dispatch = useThunkDispatch()
   const email = Cookies.get(sessionCookieName)
+  const router = useRouter()
 
   useEffect(() => {
     if (email) {
-      dispatch(login({ email, password: "mock" }))
+      dispatch(login({ email, password: "mock" })).then(() =>
+        router.push("/profile")
+      )
     }
   }, [])
 
