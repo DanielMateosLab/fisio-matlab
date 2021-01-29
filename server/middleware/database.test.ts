235da0782651440cd60fae9033833ded14c36d6c
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-01-29 22:04:40 +0100

    Database middleware ataches a close event handler to the res that closes the conn

--- server/middleware/database.test.ts
+++ server/middleware/database.test.ts
@@ -1,8 +1,7 @@
 import { MongoClient } from "mongodb"
 import httpRequest from "node-mocks-http"
 import { MissingEnvVarError } from "../../appShared/errors"
-import database, { resetConn } from "./database"
-require("dotenv").config()
+import database, { onClose, resetConn } from "./database"
 
 jest.mock("mongodb", () => ({
   MongoClient: {
@@ -20,8 +19,7 @@ describe("database", () => {
   it("should add the conn to the req object", async () => {
     await database(req, res)
 
-    // true is the return value of the mocked connect()
-    expect(req.conn).toEqual(true)
+    expect(req.conn).toBeDefined()
   })
   it("should call MongoClient.connect only in the first call", async () => {
     await database(req, res)
@@ -29,6 +27,30 @@ describe("database", () => {
 
     expect(MongoClient.connect).toHaveBeenCalledTimes(1)
   })
+  describe("should close the db conn when the response ends", () => {
+    test("the event handler should close the connection if the client is connected", () => {
+      const conn = {
+        isConnected: true,
+        close: jest.fn(async () => {}),
+      }
+
+      onClose(conn as any)()
+      expect(conn.close).toHaveBeenCalled()
+
+      conn.close.mockClear()
+      conn.isConnected = false
+      onClose(conn as any)()
+      expect(conn.close).not.toHaveBeenCalled()
+    })
+    it("should set up the onClose event handler", async () => {
+      const spy = jest.spyOn(res, "addListener")
+      await database(req, res)
+
+      expect(spy).toHaveBeenCalled()
+      expect(spy.mock.calls[0]).toContain("close")
+    })
+  })
+  // Has to be the last test to avoid a MissingEnvVarError
   it("should throw if process.env.DB_URI is not set", async () => {
     expect.hasAssertions()
     process.env.DB_URI = ""
--- server/middleware/database.ts
+++ server/middleware/database.ts
@@ -5,23 +5,29 @@ import { ExtendedRequest } from "../types"
 
 let conn: MongoClient | undefined
 
+// HOC with the conn as parameter that returns the handler. Allows us to test it with a mock conn
+export const onClose = (conn: MongoClient | undefined) => () => {
+  if (conn && conn.isConnected) {
+    conn.close().catch(() => {})
+  }
+}
+
 export default async function database(
   req: ExtendedRequest,
   res: NextApiResponse
 ): Promise<void> {
-  if (conn) {
-    req.conn = conn
-    return
+  if (!conn) {
+    const DB_URI = process.env.DB_URI
+    if (!DB_URI) throw new MissingEnvVarError("DB_URI")
+
+    conn = await MongoClient.connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
   }
-
-  const DB_URI = process.env.DB_URI
-  if (!DB_URI) throw new MissingEnvVarError("DB_URI")
-
-  conn = await MongoClient.connect(DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
   req.conn = conn
+
+  res.addListener("close", onClose(conn))
 }
 
 // For testing purposes only
