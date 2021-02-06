3ed8778d986f66aa126fe28c128b2992aee517ca
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-02-06 17:11:26 +0100

    Code clean

--- client/session/Signup.test.tsx
+++ client/session/Signup.test.tsx
@@ -1,9 +1,4 @@
-import {
-  render,
-  renderAuthenticated,
-  RenderResult,
-  waitFor,
-} from "../clientShared/testUtils"
+import { render, RenderResult, waitFor } from "../clientShared/testUtils"
 import Signup, {
   signupComponentTitle,
   emailInputText,
@@ -39,181 +34,166 @@ describe("Signup", () => {
   let queries: RenderResult
   let getByText: typeof queries.getByText
 
+  const email = "aaaaa@aaaa.com"
+  const password = "a".repeat(8)
+
   beforeEach(() => {
     queries = render(<Signup />)
     getByText = queries.getByText
   })
 
-  it("should render the signup component title", () => {
-    const titleElement = getByText(signupComponentTitle)
+  describe("signup page title", () => {
+    it("should render the signup component title", () => {
+      const titleElement = getByText(signupComponentTitle)
 
-    expect(titleElement).toBeInTheDocument()
+      expect(titleElement).toBeInTheDocument()
+    })
   })
 
-  describe("Form", () => {
-    describe("email input", () => {
-      let emailInputElement: HTMLElement
+  describe("email input", () => {
+    let emailInputElement: HTMLElement
 
-      beforeEach(() => {
-        emailInputElement = queries.getByLabelText(emailInputText)
-      })
-
-      it("should have an email input", () => {
-        expect(emailInputElement).toBeInTheDocument()
-      })
-      // The following integration tests are not necessary as validation is tested separatedly
-      test("the email validation should fail with a random word", async () => {
-        userEvent.type(emailInputElement, "aaaaa")
-        userEvent.tab()
-
-        await waitFor(() => {
-          expect(emailInputElement).toBeInvalid()
-          const errorText = queries.getByText(emailErrorText)
-          expect(errorText).toBeInTheDocument()
-        })
-      })
-      test("the email validation should fail with an empty email", async () => {
-        userEvent.type(emailInputElement, "")
-        userEvent.tab()
-
-        await waitFor(() => {
-          expect(emailInputElement).toBeInvalid()
-          const errorText = queries.getByText(requiredErrorText)
-          expect(errorText).toBeInTheDocument()
-        })
-      })
-      test("the email validation should fail with a 257 characters email", async () => {
-        userEvent.type(
-          emailInputElement,
-          // maxChars + 1 (to make the test fail) - 7 letters of the email domain part
-          "a".repeat(emailMaxCharacters + 1 - 7) + "@aa.aaa"
-        )
-        userEvent.tab()
-
-        await waitFor(() => {
-          expect(emailInputElement).toBeInvalid()
-          const errorText = getByText(getMaxErrorText(emailMaxCharacters))
-          expect(errorText).toBeInTheDocument()
-        })
-      })
-      test("the email validation should pass with a valid email", async () => {
-        userEvent.type(emailInputElement, "aaaaa@aaaa.com")
-        userEvent.tab()
-
-        await waitFor(() => {
-          expect(emailInputElement).toBeValid()
-        })
-      })
+    beforeEach(() => {
+      emailInputElement = queries.getByLabelText(emailInputText)
     })
 
-    describe("password input", () => {
-      let passwordInputElement: HTMLElement
+    it("should have an email input", () => {
+      expect(emailInputElement).toBeInTheDocument()
+    })
+    // The following integration tests are not necessary as validation is tested separatedly
+    test("the email validation should fail with a random word", async () => {
+      userEvent.type(emailInputElement, "aaaaa")
+      userEvent.tab()
 
-      beforeEach(() => {
-        passwordInputElement = queries.getByLabelText(passwordInputText)
-      })
-
-      it("should have a password input", () => {
-        expect(passwordInputElement).toBeDefined()
-      })
-      // The following integration tests are not necessary as validation is tested separatedly
-      test("the password validation should fail with no input", async () => {
-        userEvent.type(passwordInputElement, "")
-        userEvent.tab()
-
-        await waitFor(() => {
-          expect(passwordInputElement).toBeInvalid()
-          const errorText = getByText(requiredErrorText)
-          expect(errorText).toBeInTheDocument()
-        })
-      })
-      test("the password validation should fail with less characters than the minium length allowed", async () => {
-        userEvent.type(
-          passwordInputElement,
-          "a".repeat(passwordMinCharacters - 1)
-        )
-        userEvent.tab()
-
-        await waitFor(() => {
-          expect(passwordInputElement).toBeInvalid()
-          const errorText = getByText(getMinErrorText(passwordMinCharacters))
-          expect(errorText).toBeInTheDocument()
-        })
-      })
-      test("the password validation should fail with more characters than the maxium length allowed", async () => {
-        userEvent.type(
-          passwordInputElement,
-          "a".repeat(passwordMaxCharacters + 1)
-        )
-        userEvent.tab()
-
-        await waitFor(() => {
-          expect(passwordInputElement).toBeInvalid()
-          const errorText = getByText(getMaxErrorText(passwordMaxCharacters))
-          expect(errorText).toBeInTheDocument()
-        })
-      })
-      test("the password validation should pass with a 20 letters password", async () => {
-        userEvent.type(passwordInputElement, "a".repeat(20))
-        userEvent.tab()
-
-        await waitFor(() => {
-          expect(passwordInputElement).toBeValid()
-        })
+      await waitFor(() => {
+        expect(emailInputElement).toBeInvalid()
+        const errorText = queries.getByText(emailErrorText)
+        expect(errorText).toBeInTheDocument()
       })
     })
+    test("the email validation should fail with an empty email", async () => {
+      userEvent.type(emailInputElement, "")
+      userEvent.tab()
 
-    describe("repeatPassword input", () => {
-      let repeatPasswordElement: HTMLElement
-
-      beforeEach(() => {
-        repeatPasswordElement = queries.getByLabelText(repeatPasswordInputText)
-      })
-
-      it("should have a repeat password input", () => {
-        expect(repeatPasswordElement).toBeInTheDocument()
-      })
-      test("the repeatPassword validation fails with different passwords", async () => {
-        const passwordElement = queries.getByLabelText(passwordInputText)
-
-        userEvent.type(passwordElement, "mockPassword")
-        userEvent.type(repeatPasswordElement, "aaaa")
-        userEvent.tab()
-
-        await waitFor(() => {
-          expect(repeatPasswordElement).toBeInvalid()
-          const errorTextElement = getByText(repeatPasswordErrorText)
-          expect(errorTextElement).toBeInTheDocument()
-        })
-      })
-      test("the repeatPassword validation passes with the same password", async () => {
-        const passwordElement = queries.getByLabelText(passwordInputText)
-
-        const password = "mockPassword"
-        userEvent.type(passwordElement, password)
-        userEvent.type(repeatPasswordElement, password)
-        userEvent.tab()
-
-        await waitFor(() => {
-          expect(repeatPasswordElement).toBeValid()
-        })
+      await waitFor(() => {
+        expect(emailInputElement).toBeInvalid()
+        const errorText = queries.getByText(requiredErrorText)
+        expect(errorText).toBeInTheDocument()
       })
     })
+    test("the email validation should fail with a 257 characters email", async () => {
+      userEvent.type(
+        emailInputElement,
+        // maxChars + 1 (to make the test fail) - 7 letters of the email domain part
+        "a".repeat(emailMaxCharacters + 1 - 7) + "@aa.aaa"
+      )
+      userEvent.tab()
 
-    describe("login page link", () => {
-      let loginPageLinkElement: HTMLElement
-
-      beforeEach(() => {
-        loginPageLinkElement = getByText("Inicia sesión")
+      await waitFor(() => {
+        expect(emailInputElement).toBeInvalid()
+        const errorText = getByText(getMaxErrorText(emailMaxCharacters))
+        expect(errorText).toBeInTheDocument()
       })
+    })
+    test("the email validation should pass with a valid email", async () => {
+      userEvent.type(emailInputElement, email)
+      userEvent.tab()
 
-      it("should have a link to go to the login page", () => {
-        expect(loginPageLinkElement).toBeInTheDocument()
+      await waitFor(() => {
+        expect(emailInputElement).toBeValid()
       })
-      test("the link should redirect to the login page", () => {
-        userEvent.click(loginPageLinkElement)
+    })
+  })
 
-        expect(mockPush).toHaveBeenCalledWith("/login")
+  describe("password input", () => {
+    let passwordInputElement: HTMLElement
+
+    beforeEach(() => {
+      passwordInputElement = queries.getByLabelText(passwordInputText)
+    })
+
+    it("should have a password input", () => {
+      expect(passwordInputElement).toBeDefined()
+    })
+    // The following integration tests are not necessary as validation is tested separatedly
+    test("the password validation should fail with no input", async () => {
+      userEvent.type(passwordInputElement, "")
+      userEvent.tab()
+
+      await waitFor(() => {
+        expect(passwordInputElement).toBeInvalid()
+        const errorText = getByText(requiredErrorText)
+        expect(errorText).toBeInTheDocument()
+      })
+    })
+    test("the password validation should fail with less characters than the minium length allowed", async () => {
+      userEvent.type(
+        passwordInputElement,
+        "a".repeat(passwordMinCharacters - 1)
+      )
+      userEvent.tab()
+
+      await waitFor(() => {
+        expect(passwordInputElement).toBeInvalid()
+        const errorText = getByText(getMinErrorText(passwordMinCharacters))
+        expect(errorText).toBeInTheDocument()
+      })
+    })
+    test("the password validation should fail with more characters than the maxium length allowed", async () => {
+      userEvent.type(
+        passwordInputElement,
+        "a".repeat(passwordMaxCharacters + 1)
+      )
+      userEvent.tab()
+
+      await waitFor(() => {
+        expect(passwordInputElement).toBeInvalid()
+        const errorText = getByText(getMaxErrorText(passwordMaxCharacters))
+        expect(errorText).toBeInTheDocument()
+      })
+    })
+    test("the password validation should pass with a valid password", async () => {
+      userEvent.type(passwordInputElement, password)
+      userEvent.tab()
+
+      await waitFor(() => {
+        expect(passwordInputElement).toBeValid()
+      })
+    })
+  })
+
+  describe("repeatPassword input", () => {
+    let repeatPasswordElement: HTMLElement
+
+    beforeEach(() => {
+      repeatPasswordElement = queries.getByLabelText(repeatPasswordInputText)
+    })
+
+    it("should have a repeat password input", () => {
+      expect(repeatPasswordElement).toBeInTheDocument()
+    })
+    test("the repeatPassword validation fails with different passwords", async () => {
+      const passwordElement = queries.getByLabelText(passwordInputText)
+
+      userEvent.type(passwordElement, "mockPassword")
+      userEvent.type(repeatPasswordElement, "aaaa")
+      userEvent.tab()
+
+      await waitFor(() => {
+        expect(repeatPasswordElement).toBeInvalid()
+        const errorTextElement = getByText(repeatPasswordErrorText)
+        expect(errorTextElement).toBeInTheDocument()
+      })
+    })
+    test("the repeatPassword validation passes with the same password", async () => {
+      const passwordElement = queries.getByLabelText(passwordInputText)
+
+      userEvent.type(passwordElement, password)
+      userEvent.type(repeatPasswordElement, password)
+      userEvent.tab()
+
+      await waitFor(() => {
+        expect(repeatPasswordElement).toBeValid()
       })
     })
   })
@@ -232,8 +212,12 @@ describe("Signup", () => {
   })
 
   describe("api responses", () => {
-    const email = "aaaaa@aaaa.com"
-    const password = "a".repeat(8)
+    const successfulApiRes = JSON.stringify({ email })
+    const error = "mock error"
+    const validationErrorApiRes = JSON.stringify({
+      payload: { email: error },
+    })
+    const formErrorApiRes = JSON.stringify({ message: error })
 
     const submitForm = () => {
       const emailInputElement = queries.getByLabelText(emailInputText)
@@ -254,7 +238,7 @@ describe("Signup", () => {
     }
     describe("response succeeded", () => {
       it("should dispatch authFulfilled with the email", async () => {
-        fetchMock.once(JSON.stringify({ email }))
+        fetchMock.once(successfulApiRes)
         submitForm()
 
         await waitFor(() => {
@@ -262,7 +246,7 @@ describe("Signup", () => {
         })
       })
       it("should redirect to /profile", async () => {
-        fetchMock.once(JSON.stringify({ email }))
+        fetchMock.once(successfulApiRes)
         submitForm()
 
         await waitFor(() => {
@@ -272,8 +256,7 @@ describe("Signup", () => {
     })
     describe("response failed", () => {
       it("should set an email error if the response has a payload with an email property)", async () => {
-        const error = "mock validation error"
-        fetchMock.once(JSON.stringify({ payload: { email: error } }))
+        fetchMock.once(validationErrorApiRes)
         submitForm()
 
         await waitFor(() => {
@@ -282,7 +265,7 @@ describe("Signup", () => {
         })
       })
       it("should set a form error if the response has nor payload or email", async () => {
-        fetchMock.once(JSON.stringify({ message: "mock error" }))
+        fetchMock.once(formErrorApiRes)
         submitForm()
 
         await waitFor(() => {
@@ -296,24 +279,41 @@ describe("Signup", () => {
         name: submitButtonText,
       })
 
-      fetchMock.once(JSON.stringify({ email }))
+      fetchMock.once(successfulApiRes)
       submitForm()
       await waitFor(() => {
         expect(submitButtonElement).toBeEnabled()
       })
 
       const error = "mock validation error"
-      fetchMock.once(JSON.stringify({ payload: { email: error } }))
+      fetchMock.once(validationErrorApiRes)
       submitForm()
       await waitFor(() => {
         expect(submitButtonElement).toBeEnabled()
       })
 
-      fetchMock.once(JSON.stringify({ message: "mock error" }))
+      fetchMock.once(formErrorApiRes)
       submitForm()
       await waitFor(() => {
         expect(submitButtonElement).toBeEnabled()
       })
+    })
+  })
+
+  describe("login page link", () => {
+    let loginPageLinkElement: HTMLElement
+
+    beforeEach(() => {
+      loginPageLinkElement = getByText("Inicia sesión")
+    })
+
+    it("should have a link to go to the login page", () => {
+      expect(loginPageLinkElement).toBeInTheDocument()
+    })
+    test("the link should redirect to the login page", () => {
+      userEvent.click(loginPageLinkElement)
+
+      expect(mockPush).toHaveBeenCalledWith("/login")
     })
   })
 })
