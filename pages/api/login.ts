e7858082c6da80529ff952238af9f4717c8dd9d6
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-02-09 21:25:38 +0100

    Export handler wrapped in catchErrors middleware

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,11 +1,14 @@
 import { NextApiResponse } from "next"
+import catchErrors from "server/middleware/catchErrors"
 import runMiddlewares from "server/middleware/runMiddlewares"
 import session from "server/middleware/session"
-import { ExtendedRequest } from "server/types"
+import { ExtendedApiHandler, ExtendedRequest } from "server/types"
 
-export default async (req: ExtendedRequest, res: NextApiResponse) => {
+export const loginHandler: ExtendedApiHandler = async (req, res) => {
   if (req.method !== "POST") {
     res.status(405).json({ message: "Invalid method" })
   }
   await runMiddlewares(req, res, session)
 }
+
+export default catchErrors(loginHandler)
