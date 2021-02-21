2d35907fd66e996b2fc9d7adece4907c6a58b643
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-02-21 17:29:34 +0100

    /api/login does not throw with DELETE method

--- __test__/api/login.test.ts
+++ __test__/api/login.test.ts
@@ -1,57 +1,28 @@
-import * as auth from "server/auth"
-import bcrypt from "bcryptjs"
 import {
-  MethodNotAllowedError,
   InvalidCredentialsError,
+  MethodNotAllowedError,
 } from "appShared/errors"
 import { LoginData } from "appShared/types"
 import { loginValidationSchema } from "appShared/Validation"
+import bcrypt from "bcryptjs"
 import {
   createMocks,
   createRequest,
   createResponse,
+  RequestMethod,
   RequestOptions,
 } from "node-mocks-http"
 import { loginHandler } from "pages/api/login"
+import * as auth from "server/auth"
 import UsersDAO from "server/usersDAO"
 
 describe("/api/login", () => {
-  let req: any, res: any
-
-  const getUserByEmailSpy = jest
-    .spyOn(UsersDAO, "getUserByEmail")
-    .mockImplementation(async () => ({ email, password }))
-
-  const bcryptSpy = jest
-    .spyOn(bcrypt, "compare")
-    .mockImplementation(async () => true)
-
-  const user: LoginData = {
-    email: "aaaa@aaa.aa",
-    password: "bbbbbb",
-  }
-
-  const { email, password } = user
-
-  beforeEach(() => {
-    req = createRequest({
-      method: "POST",
-      body: user,
-    })
-    res = createResponse()
-  })
-
   it("should throw a MethodNotAllowedError with get, put, patch or del", async () => {
-    const notAllowedMethods: RequestOptions["method"][] = [
-      "GET",
-      "PUT",
-      "PATCH",
-      "DELETE",
-    ]
+    const notAllowedMethods: RequestMethod[] = ["GET", "PUT", "PATCH"]
+    const methods: RequestMethod[] = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    const thrownWithMethod: RequestMethod[] = []
 
-    expect.assertions(notAllowedMethods.length)
-
-    for (let method in notAllowedMethods) {
+    for (let method of methods) {
       const { req, res } = createMocks({
         method: method as RequestOptions["method"],
       })
@@ -59,81 +30,113 @@ describe("/api/login", () => {
       try {
         await loginHandler(req as any, res as any)
       } catch (e) {
-        expect(e).toBeInstanceOf(MethodNotAllowedError)
+        if (e instanceof MethodNotAllowedError) {
+          thrownWithMethod.push(method)
+        }
       }
     }
+
+    expect(thrownWithMethod).toEqual(notAllowedMethods)
   })
-  it("should validate the request body", async () => {
-    const spy = jest
-      .spyOn(loginValidationSchema, "validate")
-      .mockImplementationOnce(async (values) => values)
 
-    await loginHandler(req, res)
+  describe("POST", () => {
+    let req: any, res: any
 
-    expect(spy).toHaveBeenCalledWith(user)
-  })
-  it("should run session, database and users middlewares", async () => {
-    const sessionSpy = jest.spyOn(
-      require("server/middleware/session"),
-      "default"
-    )
-    const databaseSpy = jest.spyOn(
-      require("server/middleware/database"),
-      "default"
-    )
-    const usersSpy = jest.spyOn(require("server/middleware/users"), "default")
+    const getUserByEmailSpy = jest
+      .spyOn(UsersDAO, "getUserByEmail")
+      .mockImplementation(async () => ({ email, password }))
 
-    await loginHandler(req, res)
+    const bcryptSpy = jest
+      .spyOn(bcrypt, "compare")
+      .mockImplementation(async () => true)
 
-    expect(sessionSpy).toHaveBeenCalled()
-    expect(databaseSpy).toHaveBeenCalled()
-    expect(usersSpy).toHaveBeenCalled()
-  })
-  it("should try to find the user in the db", async () => {
-    await loginHandler(req, res)
+    const user: LoginData = {
+      email: "aaaa@aaa.aa",
+      password: "bbbbbb",
+    }
 
-    expect(getUserByEmailSpy).toHaveBeenCalledWith(email)
-  })
-  it("should throw an InvalidCredentialsError if the user does not exist", async () => {
-    expect.hasAssertions()
-    try {
-      jest
-        .spyOn(UsersDAO, "getUserByEmail")
-        .mockImplementationOnce(async () => null)
+    const { email, password } = user
+
+    beforeEach(() => {
+      req = createRequest({
+        method: "POST",
+        body: user,
+      })
+      res = createResponse()
+    })
+
+    it("should validate the request body", async () => {
+      const spy = jest
+        .spyOn(loginValidationSchema, "validate")
+        .mockImplementationOnce(async (values) => values)
 
       await loginHandler(req, res)
-    } catch (e) {
-      expect(e).toBeInstanceOf(InvalidCredentialsError)
-    }
-  })
-  it("should check if the password is true", async () => {
-    await loginHandler(req, res)
 
-    expect(bcryptSpy).toHaveBeenCalled()
-  })
-  it("should throw an InvalidCredentialsError the password is wrong", async () => {
-    jest.spyOn(bcrypt, "compare").mockImplementationOnce(async () => false)
-    expect.hasAssertions()
+      expect(spy).toHaveBeenCalledWith(user)
+    })
+    it("should run session, database and users middlewares", async () => {
+      const sessionSpy = jest.spyOn(
+        require("server/middleware/session"),
+        "default"
+      )
+      const databaseSpy = jest.spyOn(
+        require("server/middleware/database"),
+        "default"
+      )
+      const usersSpy = jest.spyOn(require("server/middleware/users"), "default")
 
-    try {
       await loginHandler(req, res)
-    } catch (e) {
-      expect(e).toBeInstanceOf(InvalidCredentialsError)
-    }
-  })
-  it("should call req.logIn if the password is valid", async () => {
-    const loginSpy = jest.spyOn(auth, "logIn")
 
-    await loginHandler(req, res)
+      expect(sessionSpy).toHaveBeenCalled()
+      expect(databaseSpy).toHaveBeenCalled()
+      expect(usersSpy).toHaveBeenCalled()
+    })
+    it("should try to find the user in the db", async () => {
+      await loginHandler(req, res)
 
-    expect(loginSpy).toHaveBeenCalled()
-  })
-  it('should return a res with 201 status and { status: "success" } as response body', async () => {
-    const jsonSpy = jest.spyOn(res, "json")
+      expect(getUserByEmailSpy).toHaveBeenCalledWith(email)
+    })
+    it("should throw an InvalidCredentialsError if the user does not exist", async () => {
+      expect.hasAssertions()
+      try {
+        jest
+          .spyOn(UsersDAO, "getUserByEmail")
+          .mockImplementationOnce(async () => null)
 
-    await loginHandler(req, res)
+        await loginHandler(req, res)
+      } catch (e) {
+        expect(e).toBeInstanceOf(InvalidCredentialsError)
+      }
+    })
+    it("should check if the password is true", async () => {
+      await loginHandler(req, res)
 
-    expect(res.statusCode).toEqual(201)
-    expect(jsonSpy).toHaveBeenCalledWith({ status: "success" })
+      expect(bcryptSpy).toHaveBeenCalled()
+    })
+    it("should throw an InvalidCredentialsError the password is wrong", async () => {
+      jest.spyOn(bcrypt, "compare").mockImplementationOnce(async () => false)
+      expect.hasAssertions()
+
+      try {
+        await loginHandler(req, res)
+      } catch (e) {
+        expect(e).toBeInstanceOf(InvalidCredentialsError)
+      }
+    })
+    it("should call req.logIn if the password is valid", async () => {
+      const loginSpy = jest.spyOn(auth, "logIn")
+
+      await loginHandler(req, res)
+
+      expect(loginSpy).toHaveBeenCalled()
+    })
+    it('should return a res with 201 status and { status: "success" } as response body', async () => {
+      const jsonSpy = jest.spyOn(res, "json")
+
+      await loginHandler(req, res)
+
+      expect(res.statusCode).toEqual(201)
+      expect(jsonSpy).toHaveBeenCalledWith({ status: "success" })
+    })
   })
 })
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,10 +1,11 @@
-import { logIn } from "server/auth"
-import bcrypt from "bcryptjs"
 import {
   InvalidCredentialsError,
   MethodNotAllowedError,
 } from "appShared/errors"
+import { LoginResponse } from "appShared/types"
 import { loginValidationSchema } from "appShared/Validation"
+import bcrypt from "bcryptjs"
+import { logIn } from "server/auth"
 import catchErrors from "server/middleware/catchErrors"
 import database from "server/middleware/database"
 import runMiddlewares from "server/middleware/runMiddlewares"
@@ -12,28 +13,30 @@ import session from "server/middleware/session"
 import users from "server/middleware/users"
 import { ExtendedApiHandler, User } from "server/types"
 import UsersDAO from "server/usersDAO"
-import { LoginResponse } from "appShared/types"
 
 export const loginHandler: ExtendedApiHandler<LoginResponse> = async (
   req,
   res
 ) => {
-  if (req.method !== "POST") throw new MethodNotAllowedError()
+  if (req.method == "POST") {
+    await loginValidationSchema.validate(req.body)
 
-  await loginValidationSchema.validate(req.body)
+    const { email, password } = req.body as User
+    await runMiddlewares(req, res, session, database, users)
 
-  const { email, password } = req.body as User
-  await runMiddlewares(req, res, session, database, users)
+    const user = await UsersDAO.getUserByEmail(email)
+    if (!user) throw new InvalidCredentialsError()
 
-  const user = await UsersDAO.getUserByEmail(email)
-  if (!user) throw new InvalidCredentialsError()
+    const isValidPwd = await bcrypt.compare(password, user.password)
+    if (!isValidPwd) throw new InvalidCredentialsError()
 
-  const isValidPwd = await bcrypt.compare(password, user.password)
-  if (!isValidPwd) throw new InvalidCredentialsError()
+    logIn(req, email)
 
-  logIn(req, email)
-
-  return res.status(201).json({ status: "success" })
+    return res.status(201).json({ status: "success" })
+  } else if (req.method == "DELETE") {
+  } else {
+    throw new MethodNotAllowedError()
+  }
 }
 
 export default catchErrors(loginHandler)
