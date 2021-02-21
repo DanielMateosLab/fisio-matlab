7e704b818cb44c70718bbc27d5c1472cad64e203
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-02-21 15:07:54 +0100

    Delete mock functionality of authenticateOnLoad

--- client/session/AuthenticateOnLoad.tsx
+++ client/session/AuthenticateOnLoad.tsx
@@ -1,22 +1,20 @@
 import Cookies from "js-cookie"
-import { login, sessionCookieName } from "./sessionSlice"
-import { useEffect } from "react"
 import { useRouter } from "next/router"
+import { useEffect } from "react"
 import { useThunkDispatch } from "../redux/store"
+import { sessionCookieName } from "./sessionSlice"
 
 // This component will in the future dispatch a getUser action, and will return a
 // "loading" component while session.getUserPending is true
 
 const AuthenticateOnLoad: React.FC = ({ children }) => {
   const dispatch = useThunkDispatch()
-  const email = Cookies.get(sessionCookieName)
+  const sessionActive = Cookies.get(sessionCookieName)
   const router = useRouter()
 
   useEffect(() => {
-    if (email) {
-      dispatch(login({ email, password: "mock" })).then(() =>
-        router.push("/profile")
-      )
+    if (sessionActive) {
+      // TODO: call the api to get the user
     }
   }, [])
 
