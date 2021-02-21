ac0754d2b2d858e859f1b28b861c8a5427a0802d
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-02-21 18:02:21 +0100

    Create auth logout function

--- server/auth.ts
+++ server/auth.ts
@@ -1,7 +1,16 @@
+import { pendingLogoutCookieName } from "appShared/appData"
+import Cookies from "js-cookie"
 import { ExtendedRequest } from "./types"
 
 export const logIn = (req: ExtendedRequest, email: string) => {
   req.session.email = email
+}
+
+export const logout = (req: ExtendedRequest) => {
+  req.session.destroy(() => {})
+  if (req.cookies[pendingLogoutCookieName] == "1") {
+    Cookies.remove(pendingLogoutCookieName)
+  }
 }
 
 export const isLoggedIn = (req: ExtendedRequest): boolean => !!req.session.email
