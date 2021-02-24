99ae5331a46cdfd45df3371f9cdca2e1ac941916
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-02-24 20:57:58 +0100

    Write tests

---
+++ client/session/AuthenticateOnLoad.test.tsx
@@ -0,0 +1,8 @@
+describe("AuthenticateOnLoad", () => {
+  it.todo("should try to get the user if there is a session active")
+  // This might be posible if the user has tried to log out but there was a network error
+  it.todo(
+    "should send a logout request to the server if tehere is a pending logout"
+  )
+  it.todo("should do nothing if there are no session related cookies")
+})
