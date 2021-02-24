ec005d393df89b5989320459abbf7f117ab1ea3b
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-02-24 21:24:36 +0100

    Add more test cases

--- client/session/ParseSessionOnLoad.test.tsx
+++ client/session/ParseSessionOnLoad.test.tsx
@@ -1,8 +1,12 @@
 describe("ParseSessionOnLoad", () => {
-  it.todo("should try to get the user if there is a session active")
+  describe("if there is a session active", () => {
+    it.todo("should try to get the user with a request to /users/me")
+    it.todo("should dispatch an authenticate action if we get an user")
+    it.todo("should dispatch a logout action if there is no user")
+  })
   // This might be posible if the user has tried to log out but there was a network error
   it.todo(
-    "should send a logout request to the server if tehere is a pending logout"
+    "should send a logout request to the server if there is a pending logout"
   )
   it.todo("should do nothing if there are no session related cookies")
 })
