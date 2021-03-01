8df8897506ab32b7d59d7bda8d7f24e865c90a6f
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-03-01 20:15:08 +0100

    Revert "Add UsersGetResponse and UserWithoutPassword types"
    This reverts commit e99d1c3b9683df9d76612dde7e156ea52ca4a145.

--- pages/api/users/me.ts
+++
@@ -1,9 +0,0 @@
-import catchErrors from "server/middleware/catchErrors"
-import { ExtendedApiHandler } from "server/types"
-
-export const meHandler: ExtendedApiHandler<UsersPostResponse> = async (
-  req,
-  res
-) => {}
-
-export default catchErrors(meHandler)
